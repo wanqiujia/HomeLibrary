21f71df29368928fbf882fcab9275f4f44a947b7
ying25135@sina.com.cn <ying25135@sina.com.cn>
2017-06-28 18:04:38 +0800

    123

--- 03 项目代码/HomeLibrary/HomeLibrary.WebAPI/Controllers/HomeController.cs
+++ 03 项目代码/HomeLibrary/HomeLibrary.WebAPI/Controllers/HomeController.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace HomeLibrary.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController :ApiController
     {
-        public ActionResult Index()
+        public void GetTaskList()
         {
-            ViewBag.Title = "Home Page";
-
-            return View();
+            
         }
     }
 }
